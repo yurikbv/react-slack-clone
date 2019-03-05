f12a82e13f911ff372db48c36a3f90be91ee248a
yurikbv <yurikbvv@gmail.com>
2019-03-06 01:54:30 +0300

    start app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+
+let config = {
+  apiKey: "AIzaSyAr5SV_tCjXalk9rmqfiXbnoP_UvicC5Jo",
+  authDomain: "react-slack-51a2a.firebaseapp.com",
+  databaseURL: "https://react-slack-51a2a.firebaseio.com",
+  projectId: "react-slack-51a2a",
+  storageBucket: "react-slack-51a2a.appspot.com",
+  messagingSenderId: "253425055268"
+};
+firebase.initializeApp(config);
+
+export default firebase;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
